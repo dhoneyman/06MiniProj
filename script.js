5d57bdf966c8dc51f1c6e2eb0cd5597bcaa20f92
Dylan Honeyman <dylan@dylans-iMac.local>
2021-11-22 11:47:32 -0600

    first fetch test

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello Dylan and Cristian!")
+function stockTicker(){
+    let stockURL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=VZYUMVJFAC3AIYQG';
+
+fetch(stockURL)
+    .then(function (responce) {
+      return responce.json();
+})
+    .then(function (stockTicker) {
+        console.log(stockTicker)
+
+} )
+}
+
+stockTicker();
