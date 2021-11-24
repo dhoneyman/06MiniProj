5e9bee3f1ad4b3d958d12b08bdcca1cc697579de
Dylan Honeyman <dylan@dylans-iMac.local>
2021-11-24 13:19:15 -0600

    got stocks to append appropriately

--- script.js
+++ script.js
@@ -1,52 +1,19 @@
-let stockName = document.querySelector('#stock-name');
-let curOpen = document.querySelector('#cur-open');
-let curClose = document.querySelector('#cur-close');
-let prevOpen = document.querySelector('#prev-open');
-let prevClose = document.querySelector('#prev-close');
-let delta = document.querySelector('#per-change');
+let stockName = document.querySelector('.stock-name');
+let curOpen = document.querySelector('.cur-open');
+let curClose = document.querySelector('.cur-close');
+let prevOpen = document.querySelector('.prev-open');
+let prevClose = document.querySelector('.prev-close');
+let delta = document.querySelector('.per-change');
+let majDiv = document.querySelector('search-stocks');
+let parentDiv = document.querySelector('parentDiv');
+let count = 0;
 
 
-
-function stockTicker(){
-    let stockSearch = document.querySelector('#stock-search').value;
-    let stockURL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stockSearch}&interval=5min&apikey=VZYUMVJFAC3AIYQG`;
-
-fetch(stockURL)
-    .then(function (responce) {
-        return responce.json();
-})
-    .then(function (stockResults) {
-        let tsdObj = stockResults['Time Series (Daily)'];
-        console.log(stockResults);
-        console.log(tsdObj);
-        let objArray = Object.entries(tsdObj);
-        let currentIndex = objArray[0];
-        let yesterIndex = objArray[1];
-        console.log(objArray);
-        console.log(yesterIndex);
-        // console.log(stockResults);
-
-        stockName.textContent = stockResults['Meta Data']['2. Symbol'];
-        curOpen.textContent = currentIndex['1']["1. open"];
-        curClose.textContent = currentIndex['1']["4. close"];
-        prevOpen.textContent = yesterIndex['1']["1. open"];
-        prevClose.textContent = yesterIndex['1']["4. close"];
-        delta.textContent = perChange(currentIndex['1']["1. open"], yesterIndex['1']["1. open"])
-        // stockName.textContent = currentIndex['1']["1. open"];
-        // stockName.textContent = currentIndex['1']["1. open"];
-        // stockName.textContent = currentIndex['1']["1. open"];
-        
-
-} )
-}
-
-<<<<<<< HEAD
 function perChange(num, old){
     return (((num - old)/old)*100);
 }
 
-// assisted
-$('#stock-btn').on('click',stockTicker)
+$('#stock-btn').on('click',renderField)
 
 // let search = "tesco"
 // let apikey = "VZYUMVJFAC3AIYQG"
@@ -57,10 +24,6 @@ $('#stock-btn').on('click',stockTicker)
 // })
 // stockTicker();
  
-=======
-stockTicker();
-
->>>>>>> main
 
 function getNews() {
     let newsURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=dqDiEwjATABt4rNeLEmrYjgPHHj7nXd7";
@@ -75,3 +38,27 @@ function getNews() {
     })
 }
 getNews();
+
+function renderField (stockResults){
+    let stockSearch = document.querySelector('#stock-search').value;
+    let stockURL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stockSearch}&interval=5min&apikey=VZYUMVJFAC3AIYQG`;
+
+fetch(stockURL)
+    .then(function (responce) {
+        return responce.json();
+})
+    .then(function (stockResults) {
+    let tsdObj = stockResults['Time Series (Daily)'];
+    let objArray = Object.entries(tsdObj);
+    let currentIndex = objArray[0];
+    let yesterIndex = objArray[1];
+
+    return $('#parentDiv').append(`<div class="result-1 stock-field field-1" data-number='1'>
+    <h2 class='stock-name' class="headline" style="display: block;">${stockResults['Meta Data']['2. Symbol']}</h2>
+    <p>Yesterdays opening Price: $${currentIndex['1']["1. open"]}</p>
+    <p>Yesterdays closing Price: $${currentIndex['1']["4. close"]}</p>    
+    <p>Previous days opening Price: $${yesterIndex['1']["1. open"]}</p>
+    <p>Previous days closing Price: $${yesterIndex['1']["4. close"]}</p>
+    <p>Percent Change: </p></div>`)
+    })
+}
