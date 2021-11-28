e85270375fd3d4277bf57b92f7c7a767741f42cf
Dylan Honeyman <dylan@dylans-iMac.local>
2021-11-28 13:41:23 -0600

    read me update and addition of light mode dark mode button

--- script.js
+++ script.js
@@ -5,15 +5,54 @@ let prevOpen = document.querySelector('.prev-open');
 let prevClose = document.querySelector('.prev-close');
 let delta = document.querySelector('.per-change');
 let majDiv = document.querySelector('search-stocks');
-let parentDiv = document.querySelector('parentDiv');
+let parentDiv = document.querySelector('#parent-div');
 let count = 0;
+
+
+
+function retrieveStorage(){
+
+    var stockNames = JSON.parse(localStorage.getItem('stockNames')) || [];
+    stockNames.forEach(getStock);
+}
+retrieveStorage();
+
+
+function stockTicker(){
+    let stockSearch = document.querySelector('#stock-search').value;
+    getStock(stockSearch);
+}
+
+function getStock(stockSearch){
+    let stockURL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stockSearch}&interval=5min&apikey=VZYUMVJFAC3AIYQG`;
+
+    fetch(stockURL)
+        .then(function (responce) {
+        return responce.json();
+    })
+        .then(function (stockResults) {
+            console.log(stockResults);
+            let tsdObj = stockResults['Time Series (Daily)'];
+            let objArray = Object.entries(tsdObj);
+            let currentIndex = objArray[0];
+            let yesterIndex = objArray[1];
+            let stockName = stockResults['Meta Data']['2. Symbol'];
+            $('#parent-div').prepend(renderField(stockResults, currentIndex, yesterIndex));
+            var stockNames = JSON.parse(localStorage.getItem('stockNames')) || [];
+            if(!stockNames.includes(stockName)){
+                stockNames.push(stockName);
+                localStorage.setItem('stockNames', JSON.stringify(stockNames));
+            }
+        })
+}
 
 
 function perChange(num, old){
     return (((num - old)/old)*100);
 }
 
-$('#stock-btn').on('click',renderField)
+// assisted
+$('#stock-btn').on('click',stockTicker)
 
 // let search = "tesco"
 // let apikey = "VZYUMVJFAC3AIYQG"
@@ -39,26 +78,53 @@ function getNews() {
 }
 getNews();
 
-function renderField (stockResults){
-    let stockSearch = document.querySelector('#stock-search').value;
-    let stockURL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stockSearch}&interval=5min&apikey=VZYUMVJFAC3AIYQG`;
+$('#delete-btn').on('click', function(){
+    console.log('test');
+    console.log(selectedStock);
+    let selectedStock = $(this).attr('data-name'); 
+    localStorage.removeItem('selectedStock');
+    }
+);
 
-fetch(stockURL)
-    .then(function (responce) {
-        return responce.json();
-})
-    .then(function (stockResults) {
-    let tsdObj = stockResults['Time Series (Daily)'];
-    let objArray = Object.entries(tsdObj);
-    let currentIndex = objArray[0];
-    let yesterIndex = objArray[1];
 
-    return $('#parentDiv').append(`<div class="result-1 stock-field field-1" data-number='1'>
-    <h2 class='stock-name' class="headline" style="display: block;">${stockResults['Meta Data']['2. Symbol']}</h2>
+
+
+// parentDiv.addEventListener('click', function(event){
+//     let element = event.target;
+//     console.log('test');
+//     if (element.matches("button") === true){
+//         console.log('test');
+//         var selectedStock = element.data('name');
+//         localStorage.removeItem('selectedStock');
+//     }
+// })
+
+function renderField (stockResults, currentIndex, yesterIndex){
+    
+    return `<div class="result-1 stock-field field-1 " data-name='${stockResults}'>
+    <div style='display:flex; justify-content:space-between'>
+        <h2 class='stock-name' class="headline" style="display: block;">${stockResults['Meta Data']['2. Symbol']}</h2>
+        <button id='delete-btn' class='delete-btn' data-name='${stockResults}'>X</button>
+    </div>
     <p>Yesterdays opening Price: $${currentIndex['1']["1. open"]}</p>
     <p>Yesterdays closing Price: $${currentIndex['1']["4. close"]}</p>    
     <p>Previous days opening Price: $${yesterIndex['1']["1. open"]}</p>
     <p>Previous days closing Price: $${yesterIndex['1']["4. close"]}</p>
-    <p>Percent Change: </p></div>`)
-    })
-}
+    <p>Percent Change: </p></div>`
+    }
+
+
+
+    let isDark = false;
+    
+    $('#dark').on('click', function(){
+        if (isDark) {
+            $('body').css({ 'background-color': '#fcfa99', color: '#4b024b' });
+            $(this).text('dark mode');
+            isDark = !isDark;
+        } else {
+             $('body').css({ 'background-color': '#634A4A', color: '#d9e9e8' });
+             $(this).text('light mode');
+             isDark = !isDark;
+         }
+    });
